d36dbfbc2480ff42553709b1e2a910a208b1d841
Lowell-37 <a2484822206@126.com>
2025-03-11 20:13:48 +0800

    init

---
+++ src/main/java/com/lowell/oj/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.lowell.oj.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lowell.oj.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
